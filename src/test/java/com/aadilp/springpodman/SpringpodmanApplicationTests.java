7bdad0330da9a05b2c258a844bd458cccf825973
Aadil Aijaz <aadilpitafi@gmail.com>
2020-12-25 02:58:29 +0100

    Write Buildah script

---
+++ src/test/java/com/aadilp/springpodman/SpringpodmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aadilp.springpodman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpodmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
